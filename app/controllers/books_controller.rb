52fed2bcd508dc33dad272c46ed871e65886a25e
jose llanos <josemiguelllanosjara@gmail.com>
2014-08-12 22:26:25 -0400

    se creo el partial form y actions destroy and create

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,9 @@
 class BooksController < ApplicationController
-  before_action :set_book, only:[:show,:edit,:update]
+  before_action :set_book, only:[:show,:edit,:update,:destroy]
   def index
 	@books = Book.all
   end
   def show
-
   end
   def edit
   end
@@ -16,6 +15,20 @@ class BooksController < ApplicationController
   	end
   end
   def new
+    @book = Book.new
+  end
+  def create
+    @book = Book.new(book_params)
+
+    if @book.save
+      redirect_to book_path(@book)
+    else
+      render 'new'
+    end
+  end
+  def destroy
+    @book.destroy
+    redirect_to books_path
   end
   private
   def set_book

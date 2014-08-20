1660c2a254c44d3ed4ff4ab0e82c3d22d121c0a8
Sebastian Vera <u.verainf@gmail.com>
2014-08-20 00:24:16 -0400

    Associate authors when creating a book

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,45 +1,55 @@
 class BooksController < ApplicationController
   before_action :set_book, only:[:show,:edit,:update,:destroy]
+
   def index
-	@books = Book.all
+    @books = Book.all
   end
+
   def show
   end
+
   def edit
   end
+
   def update
-  	if @book.update(book_params) 
-  		redirect_to @book,:notice=>'el libro fue editado con exito'
-  	else
-  		render 'edit'
-  	end
+    if @book.update(book_params)
+      redirect_to @book,:notice=>'el libro fue editado con exito'
+    else
+      render 'edit'
+    end
   end
+
   def new
     @book = Book.new()
   end
-  def create
 
-    
+  def create
     @book = Book.new(book_params)
-    p @book
-    logger.debug params.to_yaml
-    render :text => 'console'
-    return false
+    authors = Author.where(id: get_authors_ids)
+    @book.authors = authors
     if @book.save
-       redirect_to @book,:notice=>'El libro fue creado con exito'
-    else  
-    render 'new'
+      redirect_to @book,:notice=>'El libro fue creado con exito'
+    else
+      render 'new'
     end
   end
+
   def destroy
     @book.destroy
     redirect_to books_path,:notice=>'El libro fue eliminado'
   end
+
   private
-  def set_book
-  	@book = Book.find_by_id(params[:id])
+
+  def get_authors_ids
+    params.require(:book).require(:authors).permit(author_id: [])[:author_id]
   end
+
+  def set_book
+    @book = Book.find_by_id(params[:id])
+  end
+
   def book_params
-  	params.require(:book).permit(:title,:year,:ab)
+    params.require(:book).permit(:title,:year,:authors)
   end
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,11 +1,10 @@
 class Book < ActiveRecord::Base
+  has_many :abs
+  has_many :authors,:through=>:abs
 
-	has_many :abs
-	has_many :authors,:through=>:abs
-
-	validates :title, :presence=>{:message=>'no puede ser blanco'}
-	validates :year, :presence=>{:message=>'la fecha es obligatoria'}
-	validates :year, :numericality=>{:message=>'solo numeros'}
-	validates :year, :length => {:maximum=>4,:message=>'es demaciado largo'}
-	validates :year, :length => {:minimum=>1,:message=>'es demaciado corto'}
+  validates :title, :presence=>{:message=>'no puede ser blanco'}
+  validates :year, :presence=>{:message=>'la fecha es obligatoria'}
+  validates :year, :numericality=>{:message=>'solo numeros'}
+  validates :year, :length => {:maximum=>4,:message=>'es demaciado largo'}
+  validates :year, :length => {:minimum=>1,:message=>'es demaciado corto'}
 end
